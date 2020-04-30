f9ce4032b0409a7eccd00aed2a6a7554b45642a1
peyman <peymanahmadifar@yahoo.com>
2020-04-30 21:42:29 +0430

    moved swagger settings from the core app to the main project

--- core/urls.py
+++ core/urls.py
@@ -1,26 +1,8 @@
 from django.urls import path
 
-from rest_framework import permissions
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
 from . import views
 
 app_name = 'core'
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        # terms_of_service="https://www.google.com/policies/terms/",
-        # contact=openapi.Contact(email="contact@snippets.local"),
-        # license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('get-token/', views.MyObtainAuthToken.as_view()),
@@ -28,7 +10,4 @@ urlpatterns = [
     path('logout/', views.Logout.as_view()),
     path('enable-ga/', views.EnableGa.as_view()),
     path('disable-ga/', views.DisableGa.as_view()),
-    # path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
